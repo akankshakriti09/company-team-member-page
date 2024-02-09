e6196adfd467a53ecadae777c771a56d26547ff4
Akanksha <akankshakriti09@gmail.com>
2024-02-09 22:47:19 +0530

    First commit

---
+++ src/components/Services.jsx
@@ -0,0 +1,266 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+function Services() {
+  let message = `Welcome to VeriTech Software Services, where a dedicated team of professionals
+    works together to bring innovation, creativity and expertise to software
+    industry. get to know the individuals who make our company thrive.`;
+  return (
+    <section class="section-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h2 class="section-title">VeriTech Software Services</h2>
+
+            <p class="section-subtitle">{message}</p>
+          </div>
+          <div class="meet-our-team">
+            <p>Meet our Team</p>
+          </div>
+
+          {/* Leadership Management Team */}
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/teacher-drawing-vector-white-background_889056-69897.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Oliver Smith</h3>
+              <div class="team-info">
+                <p>Founder / CEO</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/businesswoman-avatar-cartoon-character-profile_18591-50377.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Emily Wilson</h3>
+              <div class="team-info">
+                <p>Founder / CEO</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/businessman-avatar-cartoon-character-profile_18591-50581.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>William Johanson</h3>
+              <div class="team-info">
+                <p>Founder / CEO</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/teacher-with-full-shirt-vector-illustration_969863-21714.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Oscar Davis</h3>
+              <div class="team-info">
+                <p>VP, Development & Growth</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/company-manager-vector-illustration-cartoon_969863-379.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>John Smith</h3>
+              <div class="team-info">
+                <p>VP, Sales & Marketing</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/vector-icon-man-wearing-glasses-tie-circular-frame_176841-6589.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Thomas Martin</h3>
+              <div class="team-info">
+                <p>VP, Finance & Operation</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Development Team */}
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/retro-man-background_1132-199.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>George Li</h3>
+              <div class="team-info">
+                <p>Team Lead, SE</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/vector-flat-icon-confident-woman-stylish-suit-with-vibrant-red-tie_176841-6900.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Ava Jones</h3>
+              <div class="team-info">
+                <p>Software Engineer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/young-businessman-business-clothes-glasses-round-avatar-face-icon-flat-style_768258-3329.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Jacob Martin</h3>
+              <div class="team-info">
+                <p>Software Engineer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/businesswoman-avatar-cartoon-character-profile_18591-50580.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Isabella Taylor</h3>
+              <div class="team-info">
+                <p>Software Engineer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-860.jpg?w=740&t=st=1707494916~exp=1707495516~hmac=4b3dbfabd6871f31e8c73467f19630fd18743838d37c7d1eb025bfb78ace6da6"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Jake Brown</h3>
+              <div class="team-info">
+                <p>Software Engineer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/free-vector/young-woman-white_25030-39552.jpg?w=740&t=st=1707495274~exp=1707495874~hmac=33f8b3548b44f06b2089da9919c4232349c28660f4cc64ce47f0d76c2b98bf54"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Victoria Jones</h3>
+              <div class="team-info">
+                <p>Software Engineer</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Sales and Marketing Team */}
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/hand-drawn-flat-stylish-cartoon-sticker-icon-concept-isolated-illustration_1016520-8671.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Jessica Martin</h3>
+              <div class="team-info">
+                <p>Team Lead</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/free-vector/isolated-young-handsome-man-set-different-poses-white-background-illustration_632498-649.jpg?w=740&t=st=1707495130~exp=1707495730~hmac=96dbe69146b6e7dd896705883c18f4d8be787094c40e5cc1ee712d52a1d70584"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Daniel Davis</h3>
+              <div class="team-info">
+                <p>Sales Officer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-855.jpg?w=740&t=st=1707494908~exp=1707495508~hmac=cb60c803fd886d7bf3e0e75ffa94aa6ea97823c3a973e5017b2887eabb0c0a68"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Alexander Roberts</h3>
+              <div class="team-info">
+                <p>Sales Officer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-853.jpg?w=740&t=st=1707494900~exp=1707495500~hmac=001f8690b0127918797d341e1670dbcd2dd929e432fc83568e36368bcbeec6e5"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Mason Wang</h3>
+              <div class="team-info">
+                <p>Marketing Officer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/cartoon-sticker-with-teacher-s-day-concept-design_730620-10099.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Sophie Miller</h3>
+              <div class="team-info">
+                <p>Marketing Officer</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-4">
+            <div class="team-item">
+              <img
+                src="https://img.freepik.com/premium-vector/businesswoman-professional-work-career_18591-5274.jpg?w=740"
+                class="team-img"
+                alt="pic"
+              />
+              <h3>Jennifer Li</h3>
+              <div class="team-info">
+                <p>Marketing Officer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Services;
